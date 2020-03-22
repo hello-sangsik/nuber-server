49fb9fcfed3df4ab686de4238c9ee3aa95769e7c
sangsik <toheart@gmail.com>
2020-03-22 17:11:22 +0900

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("It works!");
+import dotenv from "dotenv";
+import {Options} from "graphql-yoga";
+import {createConnection} from "typeorm";
+import app from "./app";
+import defaultConnectionOptions from "./ormConfig";
+
+dotenv.config();
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+    port: PORT,
+    playground: PLAYGROUND,
+    endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+
+createConnection(defaultConnectionOptions)
+    .then(() => app.start(appOptions, handleAppStart))
+    .catch(error => console.error(error));
